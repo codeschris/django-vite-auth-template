796e7d52e67d8300267ca7e7aaa01f164a4e4304
codeschris <chrismlolwa42@outlook.com>
2025-07-10 20:23:54 +0300

    feat: implement drf for endpoints docs and clean refactoring

--- web_backend/users/serializers.py
+++ web_backend/users/serializers.py
@@ -1,16 +1,22 @@
-from django.contrib.auth.models import User
+from django.contrib.auth import get_user_model
 from users.models import UserProfile
 from rest_framework.serializers import ModelSerializer, CharField
+
+User = get_user_model()
 
 class UserSerializer(ModelSerializer):
     password = CharField(write_only=True)
 
     class Meta:
         model = User
-        fields = ['id', 'username', 'email', 'password']
+        fields = ['id', 'username', 'email', 'first_name', 'last_name', 'password']
 
     def create(self, validated_data):
-        user = User.objects.create_user(**validated_data)
+        password = validated_data.pop('password', None)
+        user = User(**validated_data)
+        if password is not None:
+            user.set_password(password)
+        user.save()
         return user
 
 class UserProfileSerializer(ModelSerializer):
